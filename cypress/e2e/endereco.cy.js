26e7dd34756383c5253a32df54e50c5f74bc8bdb
SIMONE SANTOS KOVALENKINAS <simone.kovalenkinas@viavarejo.com.br>
2023-04-14 22:02:39 -0300

    aula de page objects

--- cypress/e2e/endereco.cy.js
+++ cypress/e2e/endereco.cy.js
@@ -1,4 +1,6 @@
 ///<reference types="cypress" />
+import EnderecoPage from '../support/page-objects/endereco.page'
+
 
 describe('Funcionalidade Endereços - Faturamento e Entrega', () => {
 beforeEach(() => {
@@ -9,8 +11,9 @@ beforeEach(() => {
     
 });
 
-    it('Deve fazer cadastro de faturamento com sucesso', () => {
+    it.only('Deve fazer cadastro de faturamento com sucesso', () => {
+        EnderecoPage.editarEnderecoFaturamento('Simone', 'Santos', 'Via', 'bRASIL', 'Avenida Comendador Dante Carraro', '101', 'Bauru', 'São Paulo', '06396250', '11999999999', 'simonekppx@gmail.com')
+        cy.get('.woocommerce-message').should('contain', 'Endereço alterado com sucesso.')
         
-        //cadastro de endereço
     });
 });
